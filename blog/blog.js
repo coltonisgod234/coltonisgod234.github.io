dfc48d6632b3d043957455d2934e88f998b0c7e1
Colton Wollinger <cwoll1home@gmail.com>
2024-06-26 17:45:17 -0400

    I'm adding blogs, it's sorta working!

---
+++ blog/blog.js
@@ -0,0 +1,38 @@
+function addItemToBlogList(itemname, itemUrl) {
+    const ul = document.getElementById("articleslist");
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    
+    link.href = itemUrl;  // Set the href attribute of the <a> element
+    link.textContent = itemname;  // Set the text content of the <a> element
+    
+    li.appendChild(link);  // Append the <a> element to the <li> element
+    ul.appendChild(li);  // Append the <li> element to the <ul> list
+}
+
+articles = [
+    {"name":"Blog 1","url":"../public/blogs/blog1.md"}
+]
+
+function loadArticle(filename) {
+    // Check if marked is loaded before using it
+    if (typeof marked !== 'undefined') {
+        const fileContent = fetch(`../public/blogs/${filename}`)
+        const htmlContent = marked.parse(fileContent); // Use marked.parse() for parsing
+        
+        // Display the HTML content
+        document.getElementById('articleDisplay').innerHTML = htmlContent;
+    } else {
+        console.error('Marked.js is not loaded or available.');
+    }
+}
+
+async function fetchItems() {
+    const ul = document.getElementById("articleslist");
+    while(ul.firstChild){
+        ul.removeChild(ul.firstChild);
+    }
+    articles.forEach(article => {
+        addItemToBlogList(article.name, article.url);
+    });
+}
---
+++ public/blogs/blog1.md
@@ -0,0 +1,58 @@
+regular
+*italic*
+**bold**
+***bold+italic***
+~strikethrough~
+Highlight ===Important words!!!===
+
+# Header 1
+## Header 2
+### Header 3
+### Heading with ID {#id}
+
+term
+: Definition
+
+> Quote
+
+1. Number List
+2. Number List
+3. Number List
+- Unordered List
+- Unordered List
+- Unordered List
+
+--- Rule
+
+[Link](https://www.example.com)
+![image](../../scratch.png)
+
+`Code`
+
+```Fenced code
+#include <stdio.h>
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+```
+
+Words with [^footnote]
+[^footnote] this is the footnote
+
+- [X] Completed task
+- [ ] Regular task
+- [ ] Regular task
+
+Emoji :joy:
+
+subscript H~2~0
+superscript X^2^
+
+Table:
+
+| Topic | Table |
+| ----- | ----- |
+|Tables!| Cool! |
+|Other  | Wow!  |
+| Test  | WAAA  |
